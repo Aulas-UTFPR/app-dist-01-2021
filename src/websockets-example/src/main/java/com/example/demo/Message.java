4d7f08096aefd25ef00320ceacd5abd7761c3f61
michelalbonico <bsd.albonico@gmail.com>
2021-08-12 16:18:51 -0300

    Example Websockets

---
+++ src/websockets-example/src/main/java/com/example/demo/Message.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+
+public class Message {
+
+    private String from;
+    private String text;
+    
+	public String getFrom() {
+		// TODO Auto-generated method stub
+		return from;
+	}
+	
+	public String getText() {
+		return text;
+	}
+
+    // getters and setters
+}
